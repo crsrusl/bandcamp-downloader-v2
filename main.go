49567dc7781d489bf3850463a2cce29bc6b9fca7
Chris Russell <chris.russell@havas.com>
2021-01-22 11:27:45 +0000

    optimisations

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -29,61 +30,161 @@ func main() {
 
 func getTrackList(doc *goquery.Document) {
 	doc.Find("script[data-tralbum]").Each(func(i int, s *goquery.Selection) {
-		var trackDataJson structs.TrackData
-
-		trackDataString, _ := s.Attr("data-tralbum")
-
-		err := json.Unmarshal([]byte(trackDataString), &trackDataJson)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		baseFilepath := fmt.Sprint("./", removeAlphaNum(trackDataJson.Artist), "-", removeAlphaNum(trackDataJson.Current.Title))
-		albumArtwork := fmt.Sprint("https://f4.bcbits.com/img/a", trackDataJson.Current.ArtID, "_16.jpg")
-		albumArtworkFilepath := baseFilepath + "/" + removeAlphaNum(trackDataJson.Current.Title) + ".jpg"
-
-		mp3 := structs.Mp3struct{
-			Artist:               trackDataJson.Artist,
-			AlbumTitle:           trackDataJson.Current.Title,
-			ArtID:                trackDataJson.Current.ArtID,
-			AlbumArtwork:         albumArtwork,
-			BaseFilepath:         baseFilepath,
-			AlbumArtworkFilepath: albumArtworkFilepath,
-		}
-
-		err = os.Mkdir(mp3.BaseFilepath, 0700)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		mp3.Image, err = downloadImage(mp3.AlbumArtworkFilepath, mp3.AlbumArtwork)
-		if err != nil {
-			log.Fatal(err)
-		}
-
+		var trackData structs.TrackData
 		var wg sync.WaitGroup
 		var downloads []string
+		var ticker = downloadStatus(&downloads)
+		var trackDataString, _ = s.Attr("data-tralbum")
 
-		ticker := downloadStatus(&downloads)
+		if jsonUnmarshalError := json.Unmarshal([]byte(trackDataString), &trackData); jsonUnmarshalError != nil {
+			log.Fatal(jsonUnmarshalError)
+		}
 
-		for _, v := range trackDataJson.Trackinfo {
+		trackData.BaseFilepath = "./" + removeAlphaNum(trackData.Artist) + removeAlphaNum(trackData.Current.Title)
+		trackData.AlbumArtwork = "https://f4.bcbits.com/img/a" + strconv.Itoa(trackData.Current.ArtID) + "_16.jpg"
+		trackData.AlbumArtworkFilepath = trackData.BaseFilepath + "/" + removeAlphaNum(trackData.Current.Title) + ".jpg"
+
+		if osMkdirError := os.Mkdir(trackData.BaseFilepath, 0700); osMkdirError != nil {
+			log.Fatal(osMkdirError)
+		}
+
+		if downloadImageError := downloadImage(trackData.AlbumArtworkFilepath, trackData.AlbumArtwork); downloadImageError != nil {
+			log.Fatal(downloadImageError)
+		}
+
+		for _, v := range trackData.Trackinfo {
 			wg.Add(1)
-			mp3.Title = v.Title
-			filePath := mp3.BaseFilepath + "/" + removeAlphaNum(mp3.Artist) + "-" + removeAlphaNum(mp3.Title) + ".mp3"
-			go downloadMp3(filePath, v.File.Mp3128, mp3, &wg, &downloads)
+			trackData.CurrentTrackTitle = v.Title
+			trackData.CurrentTrackURL = v.File.Mp3128
+			trackData.CurrentTrackFilepath = trackData.BaseFilepath + "/" + removeAlphaNum(trackData.Artist) + "-" + removeAlphaNum(trackData.CurrentTrackTitle) + ".mp3"
+			go downloadMp3(trackData, &wg, &downloads)
 		}
 
 		wg.Wait()
 
-		err = os.Remove(mp3.Image)
-		if err != nil {
-			log.Fatal(err)
+		if osRemoveError := os.Remove(trackData.AlbumArtworkFilepath); osRemoveError != nil {
+			log.Fatal(osRemoveError)
 		}
 
 		ticker.Stop()
 
 		fmt.Println("\r...Done")
 	})
+}
+
+func getArtistPage(url string) {
+	fmt.Println("Getting... ", url)
+
+	res, httpGetError := http.Get(url)
+	if httpGetError != nil {
+		log.Fatal(httpGetError)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, documentReaderError := goquery.NewDocumentFromReader(res.Body)
+	if documentReaderError != nil {
+		log.Fatal(documentReaderError)
+	}
+
+	getTrackList(doc)
+}
+
+func downloadMp3(mp3 structs.TrackData, wg *sync.WaitGroup, downloads *[]string) {
+	defer wg.Done()
+
+	*downloads = append(*downloads, fmt.Sprintf("%s - %s", mp3.Artist, mp3.CurrentTrackTitle))
+
+	resp, err := http.Get(mp3.CurrentTrackURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(mp3.CurrentTrackFilepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if tagFileError := tagFile(mp3); tagFileError != nil {
+		log.Fatal(tagFileError)
+	}
+}
+
+func tagFile(mp3 structs.TrackData) error {
+	tag, err := id3v2.Open(mp3.CurrentTrackFilepath, id3v2.Options{Parse: true})
+	if err != nil {
+		return err
+	}
+
+	artwork, err := ioutil.ReadFile(mp3.AlbumArtworkFilepath)
+	if err != nil {
+		return err
+	}
+
+	pic := id3v2.PictureFrame{
+		Encoding:    id3v2.EncodingUTF8,
+		MimeType:    "image/jpeg",
+		PictureType: id3v2.PTFrontCover,
+		Description: "Front cover",
+		Picture:     artwork,
+	}
+
+	tag.AddAttachedPicture(pic)
+	tag.SetArtist(mp3.Artist)
+	tag.SetTitle(mp3.CurrentTrackTitle)
+	tag.SetAlbum(mp3.Current.Title)
+
+	if err = tag.Save(); err != nil {
+		return err
+	}
+
+	if err = tag.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func downloadImage(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func removeAlphaNum(text string) string {
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	processedString := reg.ReplaceAllString(text, "")
+
+	return processedString
 }
 
 func downloadStatus(downloads *[]string) *time.Ticker {
@@ -109,109 +210,4 @@ func downloadStatus(downloads *[]string) *time.Ticker {
 	}()
 
 	return ticker
-}
-
-func getArtistPage(url string) {
-	fmt.Println("Getting... ", url)
-
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	getTrackList(doc)
-}
-
-func downloadMp3(filepath string, url string, mp3 structs.Mp3struct, wg *sync.WaitGroup, downloads *[]string) {
-	defer wg.Done()
-
-	*downloads = append(*downloads, fmt.Sprintf("%s - %s", mp3.Artist, mp3.Title))
-
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	out, err := os.Create(filepath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tag, err := id3v2.Open(filepath, id3v2.Options{Parse: true})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	artwork, err := ioutil.ReadFile(mp3.Image)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pic := id3v2.PictureFrame{
-		Encoding:    id3v2.EncodingUTF8,
-		MimeType:    "image/jpeg",
-		PictureType: id3v2.PTFrontCover,
-		Description: "Front cover",
-		Picture:     artwork,
-	}
-
-	tag.AddAttachedPicture(pic)
-	tag.SetArtist(mp3.Artist)
-	tag.SetTitle(mp3.Title)
-	tag.SetAlbum(mp3.AlbumTitle)
-
-	if err = tag.Save(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err = tag.Close(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func downloadImage(filepath string, url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	out, err := os.Create(filepath)
-	if err != nil {
-		return "", err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return filepath, nil
-}
-
-func removeAlphaNum(text string) string {
-	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
-	if err != nil {
-		log.Fatal(err)
-	}
-	processedString := reg.ReplaceAllString(text, "")
-
-	return processedString
 }
